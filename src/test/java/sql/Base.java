3dcd24607ef43a9746fc2753348a6a0b84e78e6a
Uzair Khan <ukhan@inspiredtesting.com>
2023-09-27 09:53:32 +0200

    added base class as utility class

---
+++ src/test/java/sql/Base.java
@@ -0,0 +1,49 @@
+package sql;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import java.sql.*;
+
+public class Base {
+    private static String DBconnUrl = "jdbc:mysql://sqljavaproject_oldcrowdno:92cd9151ff3473e5107253cbba84374d998eb545@58e.h.filess.io:3307/sqljavaproject_oldcrowdno";
+    private static String DBusername = "sqljavaproject_oldcrowdno";
+    private static String DBpassword = "92cd9151ff3473e5107253cbba84374d998eb545";
+
+
+    public static Connection connectToDB() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(DBconnUrl, DBusername, DBpassword);
+            if (conn != null) {
+                System.out.println("Connected to server");
+            } else {
+                System.out.println("No connection");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static ResultSet executeQuery(Connection conn, String sqlStatement) {
+        ResultSet res = null;
+        try {
+            Statement statement = conn.createStatement();
+            res = statement.executeQuery(sqlStatement);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
--- src/test/java/sql/SqlTest.java
+++ src/test/java/sql/SqlTest.java
@@ -2,16 +2,31 @@ package sql;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.*;
 import java.util.Scanner;
 
-public class SqlTest {
+public class SqlTest extends Base{
 
     // Retrieve the number of customers from the database
     // Check that the total number of customers is equal to 2
     @Test
     public void getAllCustomers_checkNumber_shouldBeTwo() {
+        String sqlStatement = "SELECT customer_id FROM customer_details";
 
+        try (Connection conn = connectToDB()) {
+            ResultSet result = executeQuery(conn, sqlStatement);
+
+            while (result != null && result.next()) {
+                int id = result.getInt("customer_id");
+                System.out.println("ID: " + id);
+                // You can add your validation logic here
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+
 
     // Retrieve all accounts for customer Sarah from the database
     // Check that the total number of accounts for her is 2
@@ -27,14 +42,16 @@ public class SqlTest {
     public void retrieveTransactionsForAccount_checkTotalBalance_shouldBeZero() {
 
     }
-    @Test
-    public void scanner () {
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter customerID");
-
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input
-    }
+//    public String scanner () {
+//
+//        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+//        System.out.println("Enter customerID");
+//        String id = scanner.next();
+//
+//        return id;
+////        String userName = myObj.nextLine();  // Read user input
+////        System.out.println("Username is: " + userName);  // Output user input
+//    }
 }
 
